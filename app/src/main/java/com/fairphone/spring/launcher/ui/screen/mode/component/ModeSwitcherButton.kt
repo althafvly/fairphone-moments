068a444b6e8bee7b5772168326bf2ef8c82272a4
Eduardo Pinar Menoyo <eduardo@fairphone.com>
2025-05-15 13:09:36 +0200

    Add missing translations

--- app/src/main/java/com/fairphone/spring/launcher/ui/screen/mode/component/ModeSwitcherButton.kt
+++ app/src/main/java/com/fairphone/spring/launcher/ui/screen/mode/component/ModeSwitcherButton.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025. Fairphone B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fairphone.spring.launcher.ui.screen.mode.component
 
 import androidx.compose.foundation.BorderStroke
@@ -118,7 +134,7 @@ fun ModeSwitcherButton(
 
             ActionButton(
                 icon = SettingsIcon,
-                description = stringResource(R.string.mode_switcher_screen_settings),
+                description = stringResource(R.string.setting_title_customization_settings),
                 isSelected = isSelected,
             ) {
                 onModeSettingsClick(profile)
