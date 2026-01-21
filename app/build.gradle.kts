fb83cc8386f9f65544084b01e075a851f0758d1a
althafvly <althafvly@gmail.com>
2026-01-21 20:55:40 +0530

    Change packagename to avoid conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.fairphone.spring.launcher"
+        applicationId = "org.thayyil.spring.launcher"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = appVersionCode
